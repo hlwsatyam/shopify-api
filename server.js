67207ecaf7ca77c59e523866595f3bf18b699de8
hlwsatyam <satyampandit021@gmail.com>
2025-05-27 08:52:16 +0530

    first commitggh

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 const s = require("dotenv").config();
 
 const app = express();
-const PORT = 3001;
+const PORT =  process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
